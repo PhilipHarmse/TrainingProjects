d8ec768f491b474cffba3e5847ddf41567770e60
Philip Harmse <philip.harmse@gmail.com>
2015-12-29 18:10:15 +0100

    finished with his example

--- ExtentionMethods/Program.cs
+++ ExtentionMethods/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace ExtentionMethods
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            int i = 7;
+
+
+
+            // Print(Bonus(Decrement(Increment(i))));
+
+            i.Increment().Decrement().Bonus().Print();
+
+
+            Console.ReadLine();
+
+            
+
+        }
+
+        static void Print(this object o)
+        {
+            Console.WriteLine("Called Print {0}",o.ToString());
+        }
+
+        static int Increment(this int i)
+        {
+            return i++;
+        }
+
+        static int Decrement(this int i)
+        {
+            return i--;
+        }
+
+        static int Bonus(this int i)
+        {
+            return i + 5;
         }
     }
 }
