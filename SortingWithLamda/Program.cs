65b72a69f483bc86d928ad1135a11bd33a09943b
Philip Harmse <philip.harmse@gmail.com>
2015-12-30 21:54:57 +0100

    last commit of the day

--- SortingWithLamda/Program.cs
+++ SortingWithLamda/Program.cs
@@ -11,5 +11,24 @@ namespace SortingWithLamda
         static void Main(string[] args)
         {
         }
+
+        public IEnumerable<T> Sorting<T>(IEnumerable<T> collectionToBeSorted, Func<(T newItem,T toComapreWith), bool> sortingMethod)
+        {
+            for (int i = 0; i < collectionToBeSorted.Count(); i++)
+            {
+                var PosnewItem = collectionToBeSorted.ElementAt(i + 1) ;
+                var PosItemToCompareWith = collectionToBeSorted.ElementAt(i);
+
+
+
+
+                do while (PosnewItem > PosItemToCompareWith)
+                    {
+                        collectionToBeSorted.indexOf(i) = PosnewItem;
+                        collectionToBeSorted.(i + 1) = PosItemToCompareWith;
+                    }
+
+            }
+        }
     }
 }
